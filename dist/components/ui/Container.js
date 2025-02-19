89798c8c3c8bf0a0b383327f8a0cd5d036e8b156
Will Bowman <gcphost@gmail.com>
2025-02-19 14:01:34 -0800

    Failed symlinks with nextjs, moving on

--- dist/components/ui/Container.js
+++ dist/components/ui/Container.js
@@ -1,5 +1,5 @@
-import { jsx as _jsx } from "react/jsx-runtime";
+import { jsxs as _jsxs } from "react/jsx-runtime";
 export default function Container({ children, className = "", }) {
-    return (_jsx("div", { className: `border-debug-dashed container mx-auto py-10 md:py-24 lg:px-20 print:py-0 print:px-0 ${className}`, children: children }));
+    return (_jsxs("div", { className: `border-debug-dashed container mx-auto py-10 md:py-24 lg:px-20 print:py-0 print:px-0 ${className}`, children: ["545545", children] }));
 }
 //# sourceMappingURL=Container.js.map
